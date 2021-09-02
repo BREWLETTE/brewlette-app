07f8aecf184ebe890043c5fb601ad15b4da2f5a6
Triana Cerda <trianaRcerda@gmail.com>
2021-09-01 18:14:24 -0700

    added beer pics

--- src/About.js
+++ src/About.js
@@ -9,8 +9,9 @@ class About extends Component {
             <p>Brewlette is a major problem solving app built for you by major problem solvers. Have you ever been in a cool new or previous spot but no one will make a deicision on where to grab a brew? You (21 and over) are who we had in mind when we created this app!! Brewlette is a roulette randomizer that will get you a place to enjoy a drink in no time. With a couple clicks you will be able to get some details about a brewery picked by yours truly, Brewlette. It’s as simple as logging in or signing up(if you’re 21 or over), inputting your location, and clicking spin! There are features for you to save breweries you enjoyed so you can visit them again! Brewlette, version 1.0.0 — solving your brewery selections spin by spin.</p>
             <div className='name'>
                 <h4>Triana</h4>
+                <img src='../src/assets/beerIcon1.png' alt ='beer'/>
                 <a href="http://www.linkedin.com/in/triana-cerda/">
-                <img src="../assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
+                <img src="../src/assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
                 </a>
 
                 <a class="github-button" href="https://github.com/trianacerda" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @trianacerda on GitHub">
@@ -20,8 +21,9 @@ class About extends Component {
 
             <div className='name'>
                 <h4>Cristian</h4>
+                    <img src='../src/assets/beerIcon2.png' alt ='beer'/>
                     <a href="https://www.linkedin.com/in/cristianmontesmujica/">
-                    <img src="../assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
+                    <img src="../src/assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
                     </a>
 
                     <a class="github-button" href="https://github.com/cristian-montes" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @Cristian Montes on GitHub">
@@ -31,8 +33,9 @@ class About extends Component {
 
             <div className='name'>
                 <h4>Simon</h4>
+                    <img src='../src/assets/beerIcon3.png' alt ='beer'/>
                     <a href="https://www.linkedin.com/in/simonbishopkaine/">
-                    <img src="../assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
+                    <img src="../src/assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
                     </a>
 
                     <a class="github-button" href="https://github.com/simonkaine" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @simonkaine on GitHub">
@@ -41,9 +44,10 @@ class About extends Component {
             </div>
 
             <div className='name'>
-                <h4>Aemilius</h4>
+                <h4>Aemilius</h4>\
+                    <img src='../src/assets/beerIcon4.png' alt ='beer'/>
                     <a href="https://www.linkedin.com/in/aemilius-morgan-1b0172171/">
-                    <img src="../assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
+                    <img src="../src/assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
                     </a>
 
                     <a class="github-button" href="https://github.com/AemiliusM" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @AemiliusM on GitHub">
@@ -53,8 +57,9 @@ class About extends Component {
 
             <div className='name'>
                 <h4>Alice</h4>
+                    <img src='../src/assets/beerIcon5.png' alt ='beer'/>
                     <a href="http://www.linkedin.com/in/aliceretz/">
-                    <img src="../assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
+                    <img src="../src/assets/linkdinbtn.jpg" alt="Visit my LinkedIn Profile"/>
                     </a>
 
                     <a class="github-button" href="https://github.com/Alice-Retz" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @Alice Retz(she/they) on GitHub">
