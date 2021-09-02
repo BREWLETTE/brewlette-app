e8cdec9e75babc3c86b291e0a273a5821189f76c
Triana Cerda <trianaRcerda@gmail.com>
2021-09-01 18:09:09 -0700

    skeleton about us page

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -15,7 +15,12 @@ function Spinner(props) {
         { option: 'brewlette', style: { backgroundColor: '#580F01',textColor: 'white' } },
         { option: 'brewlette', style: { backgroundColor: '#782305',textColor: 'white' } },
         { option: 'brewlette', style: { backgroundColor: '#C7941D',textColor: 'white' } },
-
+        { option: 'brewlette', style: { backgroundColor: '#1C2018', textColor: 'white' } },
+        { option: 'brewlette', style: { backgroundColor: '#0B1953',textColor: 'white' } },
+        { option: 'brewlette', style: { backgroundColor: '#43281C',textColor: 'white' } },
+        { option: 'brewlette', style: { backgroundColor: '#580F01',textColor: 'white' } },
+        { option: 'brewlette', style: { backgroundColor: '#782305',textColor: 'white' } },
+        { option: 'brewlette', style: { backgroundColor: '#C7941D',textColor: 'white' } },
         { option: 'brewlette', style: { backgroundColor: '#1C2018', textColor: 'white' } },
         { option: 'brewlette', style: { backgroundColor: '#0B1953',textColor: 'white' } },
         { option: 'brewlette', style: { backgroundColor: '#43281C',textColor: 'white' } },
