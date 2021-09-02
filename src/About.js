b31767dabe7f4f848e792bd2996b00a65e1e57e4
Alice-Retz <alicegretz@gmail.com>
2021-09-02 17:31:56 -0500

    About page styling and minor edits.

--- src/About.js
+++ src/About.js
@@ -14,14 +14,19 @@ class About extends Component {
     render() { 
         return ( 
         <>
-        <NavLink to ='/roulette'>Go Back Home</NavLink>
         <section className="container">
-            <p>Brewlette is a major problem solving app built for you by major problem solvers. Have you ever been in a cool new or previous spot but no one will make a deicision on where to grab a brew? You (21 and over) are who we had in mind when we created this app!! Brewlette is a roulette randomizer that will get you a place to enjoy a drink in no time. With a couple clicks you will be able to get some details about a brewery picked by yours truly, Brewlette. It’s as simple as logging in or signing up(if you’re 21 or over), inputting your location, and clicking spin! There are features for you to save breweries you enjoyed so you can visit them again! Brewlette, version 1.0.0 — solving your brewery selections spin by spin.</p>
+        <NavLink to ='/'>Go Back Home</NavLink>
+            <section className="mission">
+                <p>Brewlette is a major problem solving app built for you by major problem solvers. Have you ever been in a cool new or previous spot but no one will make a deicision on where to grab a brew? You (21 and over) are who we had in mind when we created this app!! Brewlette is a roulette randomizer that will get you a place to enjoy a drink in no time. With a couple clicks you will be able to get some details about a brewery picked by yours truly, Brewlette. It’s as simple as logging in or signing up(if you’re 21 or over), inputting your location, and clicking spin! There are features for you to save breweries you enjoyed so you can visit them again! Brewlette, version 1.0.0 — solving your brewery selections spin by spin.</p>
+            </section>
             
             <section className='name-section'>
-                <img src={ beerIcon1 } alt ='beer' className='beer'/>
-                <section>
-                    <h4>Triana</h4>
+                <section className='beer'>
+                <img src={ beerIcon1 } alt ='beer'/>
+                </section>
+                <section className="bio">
+                    <h4>Triana Cerda</h4>
+                    <section>
                     <a className='linkdin' href="http://www.linkedin.com/in/triana-cerda/">
                         <img src={ linkdinbtn } alt="Visit my LinkedIn Profile"/>
                     </a>
@@ -32,13 +37,17 @@ class About extends Component {
                         aria-label="Follow @trianacerda on GitHub"> 
                         <img src={ GitHub_Logo} alt='github'/>
                     </a>
+                    </section>
                 </section>
             </section>
 
             <section className='name-section'>
-                <img src={ beerIcon2 } alt ='beer' className='beer'/>
-                <section>
-                    <h4>Cristian</h4>
+                <section className='beer'>
+                    <img src={ beerIcon2 } alt ='beer' />
+                </section>
+                <section className="bio">
+                    <h4>Cristian Montes</h4>
+                    <section>
                     <a className='linkdin' href="https://www.linkedin.com/in/cristianmontesmujica/">
                         <img src={ linkdinbtn } alt="Visit my LinkedIn Profile" />
                     </a>
@@ -49,13 +58,17 @@ class About extends Component {
                         aria-label="Follow @Cristian Montes on GitHub">
                         <img src={ GitHub_Logo} alt='github'/>
                     </a>
+                    </section>
                 </section>
             </section>
 
             <section className='name-section'>
-                <img src={ beerIcon3 } alt ='beer' className='beer'/>
-                    <section>
-                        <h4>Simon</h4>
+                <section className='beer'>
+                    <img src={ beerIcon3 } alt ='beer' />
+                </section>
+                    <section className="bio">
+                        <h4>Simon Kaine</h4>
+                        <section>
                         <a className='linkdin' href="https://www.linkedin.com/in/simonbishopkaine/">
                             <img src={ linkdinbtn } alt="Visit my LinkedIn Profile" />
                         </a>
@@ -66,13 +79,38 @@ class About extends Component {
                             aria-label="Follow @simonkaine on GitHub">
                             <img src={ GitHub_Logo} alt='github'/>
                         </a>
+                        </section>
                     </section>
             </section>
 
             <section className='name-section'>
-                <img src={ beerIcon5 } alt ='beer' className='beer'/>   
-                <section>
-                    <h4>Alice</h4>
+                <section className='beer'>
+                    <img src={ beerIcon4 } alt ='beer'/>
+                </section>
+                    <section className="bio">
+                        <h4>Aemilius Morgan</h4>
+                        <section>
+                        <a className='linkdin' href="https://www.linkedin.com/in/aemilius-morgan-1b0172171/">
+                        <img src={ linkdinbtn } alt="Visit my LinkedIn Profile" />
+                        </a>
+
+                        <a className="github-button" 
+                            href="https://github.com/AemiliusM" 
+                            data-color-scheme="no-preference: dark; light: dark; dark: dark;" 
+                            aria-label="Follow @AemiliusM on GitHub">
+                            <img src={ GitHub_Logo} alt='github'/>
+                        </a>
+                        </section>
+                    </section> 
+            </section>
+
+            <section className='name-section'>
+                <section className='beer'>
+                    <img src={ beerIcon5 } alt ='beer' /> 
+                </section>  
+                <section className="bio">
+                    <h4>Alice Retz</h4>
+                    <section>
                     <a className='linkdin' href="http://www.linkedin.com/in/aliceretz/">
                         <img src={ linkdinbtn } alt="Visit my LinkedIn Profile" />
                     </a>
@@ -83,6 +121,7 @@ class About extends Component {
                         aria-label="Follow @Alice Retz(she/they) on GitHub">
                         <img src={ GitHub_Logo} alt='github'/>
                     </a>
+                    </section>
 
                     {/* <a 
                         href="https://twitter.com/AliceRetz?ref_src=twsrc%5Etfw" 
@@ -97,24 +136,6 @@ class About extends Component {
                     </script> */}
                 </section>   
             </section> 
-
-            <section className='name-section'>
-                <img src={ beerIcon4 } alt ='beer' className='beer'/>
-                    <section>
-                        <h4>Aemilius</h4>
-                        <a className='linkdin' href="https://www.linkedin.com/in/aemilius-morgan-1b0172171/">
-                        <img src={ linkdinbtn } alt="Visit my LinkedIn Profile" />
-                        </a>
-
-                        <a className="github-button" 
-                            href="https://github.com/AemiliusM" 
-                            data-color-scheme="no-preference: dark; light: dark; dark: dark;" 
-                            aria-label="Follow @AemiliusM on GitHub">
-                            <img src={ GitHub_Logo} alt='github'/>
-                        </a>
-                    </section> 
-            </section>
-
 
         </section>
         </>
